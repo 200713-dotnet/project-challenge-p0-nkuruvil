51d2a415052883c48dec41972229e525a767c842
nkuruvil <noahk0597@gmail.com>
2020-07-21 13:59:54 -0500

    Added files

---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaBox.client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PizzaBox.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PizzaBox.domain
+{
+    public class Class1
+    {
+    }
+}
